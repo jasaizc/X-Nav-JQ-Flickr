7edd931dd1d952ca97e053a977536c7431d5a9ac
Jesus Alberto Saiz <ja.saizc@alumnos.urjc.es>
2015-03-03 14:41:00 +0100

    Terminado con opcion de elegir Tag

--- prueba.js
+++ prueba.js
@@ -2,15 +2,25 @@ var words;
 
 jQuery(document).ready(function() {  
    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+   
+  $("#boton1").click(function(){
    $.getJSON( flickerAPI, {
-   tags: "fuenlabrada",
+   tags: $("#Resultado").val(),
    tagmode: "any",
    format: "json"
     })
+  
 .done(function( data ) {
   words = data;
-  $("#boton").click(cambiafrase);       
-})
+
+  $("#boton").click(function(){
+    aleat = parseInt(Math.random()*words.items.length) 
+    changer(words.items[aleat].media.m) 
+  
+  });
+          
+});
+});
   function changer (newValue) {
   var element = document.getElementById("imagen1");
   element.style.display = 'inline';
